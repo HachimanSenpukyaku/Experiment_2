16cd68ac6e7b223a69a52a1c19dda48fca4bfc43
Ryo Villeza <47406158+HachimanSenpukyaku@users.noreply.github.com>
2019-05-03 19:54:12 +0800

    Add files via upload

---
+++ RyoProblem7.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N,Sum;
+	
+	
+	
+	cout<<"Enter a Number: ";
+	cin>>N;
+	
+	if (N>0)
+	{
+	while (N>0)
+	{
+		for(int a=1; a<=N; ++a)
+		Sum += a;
+		cout<<"the sum of all numbers from 1 to "<< N <<" is "<< Sum <<endl;
+        cout<<"\nEnter a Number: ";
+        cin >> N;
+        
+        Sum = 0;
+    }
+         cout<<"\n";
+    }
+    
+    while (N<=0)
+    {
+    	cout << "Thank You!" << endl;
+    	break;
+	}
+  
+	
+	return 0;
+	
+}
